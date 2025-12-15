e7324b5b75ed106b05c3901d17a80671f6df1113
TheDiVaZo <thedivazo.staff@exbo.ru>
2025-12-15 23:47:42 +0300

    Add `getPlayer` in PlatformAdapter

--- src/main/java/me/thedivazo/bubbleoverhead/common/adapter/PlatformAdapter.java
+++ src/main/java/me/thedivazo/bubbleoverhead/common/adapter/PlatformAdapter.java
@@ -1,13 +1,17 @@
 package me.thedivazo.bubbleoverhead.common.adapter;
 
 import me.thedivazo.bubbleoverhead.common.MinecraftVersion;
+import me.thedivazo.bubbleoverhead.common.Viewer;
 import me.thedivazo.bubbleoverhead.common.World;
 import me.thedivazo.bubbleoverhead.common.math.Location;
 
+import java.util.UUID;
+
 public interface PlatformAdapter {
-    MinecraftVersion getCurrentVersion();
+    MinecraftVersion currentVersion();
 
     Object asPlatformWorld(World world);
     Object fromLocation(Location location);
+    Object getPlayer(UUID uuid);
 
 }
