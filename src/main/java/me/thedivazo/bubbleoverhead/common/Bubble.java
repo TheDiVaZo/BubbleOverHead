bc115b5dbf96107406f284cedd1f2c3ed30af3b2
TheDiVaZo <thedivazo.staff@exbo.ru>
2025-12-15 23:45:47 +0300

    Bubble has been updated with new methods

--- src/main/java/me/thedivazo/bubbleoverhead/common/Bubble.java
+++ src/main/java/me/thedivazo/bubbleoverhead/common/Bubble.java
@@ -1,9 +1,14 @@
 package me.thedivazo.bubbleoverhead.common;
 
+import me.thedivazo.bubbleoverhead.common.math.Vector;
+
 public interface Bubble {
     Message message();
+    void setMessage(Message message);
     void show(Viewer viewer);
     void update();
     void hide(Viewer viewer);
     void hideAll();
+    Vector location();
+    void setLocation(Vector vector);
 }
