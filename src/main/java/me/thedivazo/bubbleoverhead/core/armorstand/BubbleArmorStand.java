e992e44247de24b2c6cb3fa094d92d34f4674836
TheDiVaZo (server) <thedivazoyoutub@gmail.com>
2025-12-15 20:24:45 +0300

    Create bubble armor stand class

---
+++ src/main/java/me/thedivazo/bubbleoverhead/core/armorstand/BubbleArmorStand.java
@@ -0,0 +1,37 @@
+package me.thedivazo.bubbleoverhead.core.armorstand;
+
+import me.thedivazo.bubbleoverhead.common.Bubble;
+import me.thedivazo.bubbleoverhead.common.Message;
+import me.thedivazo.bubbleoverhead.common.Viewer;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Minecart;
+
+public class BubbleArmorStand implements Bubble {
+
+    private final int version =
+
+    @Override
+    public Message message() {
+        return null;
+    }
+
+    @Override
+    public void show(Viewer viewer) {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void hide(Viewer viewer) {
+
+    }
+
+    @Override
+    public void hideAll() {
+
+    }
+}
