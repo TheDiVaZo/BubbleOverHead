acba95ec8210d0d47a96241700d6c9f978b5290a
TheDiVaZo (server) <thedivazoyoutub@gmail.com>
2025-12-15 20:25:28 +0300

    Create BubbleOverHead class extends JavaPlugin

---
+++ src/main/java/me/thedivazo/bubbleoverhead/BubbleOverHead.java
@@ -0,0 +1,17 @@
+package me.thedivazo.bubbleoverhead;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BubbleOverHead extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
